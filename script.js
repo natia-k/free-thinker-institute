a3654ba08b02d4d9c39dd6d0667c4bb3a90d973e
natia-k <natia.kurtanidze55@gmail.com>
2026-04-12 21:16:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -230,7 +230,6 @@ if (heroDashboardPanel) {
 
   heroDashboardPanel.addEventListener("mousemove", (e) => {
     const rect = heroDashboardPanel.getBoundingClientRect();
-
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
 
